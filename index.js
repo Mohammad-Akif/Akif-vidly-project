8406a4eb1f452bcbf8d179edb722dcace144b6e5
qureshius <khurram@suprize.com>
2020-12-20 00:20:48 +0500

    changed messages

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ require("./startup/validation")();
 
 const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () =>
-  winston.info(`Listening on port ${port}...`)
+  winston.info(`Sever is istening on port ${port}...`)
 );
 
 
